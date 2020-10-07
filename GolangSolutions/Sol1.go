23f273611c58269b735a9b98b2e88988a80c6941
Kumar Abhishek <43039927+KodeSaga@users.noreply.github.com>
2020-10-07 17:13:56 +0530

    Updated some formatting , removed comment and changed a few steps

--- GolangSolutions/Sol1.go
+++ GolangSolutions/Sol1.go
@@ -1,3 +1,5 @@
+//Solution 1 - Time Complexity O(1)
+//
 package main
 
 import (
@@ -7,38 +9,33 @@ import (
 
 func termCount(a int, b int) int{
   
-  // finding n of nth term(First valid term) of type  x*(x+1) in the range [A ... B]
+  	// finding n of nth term(First valid term) of type  x*(x+1) in the range [A ... B]
 	tempFirstPos := int(math.Floor(math.Sqrt(float64(a))))
 	firstPos := tempFirstPos
-	if tempFirstPos*(tempFirstPos+1) < a {
-	  firstPos = tempFirstPos+1
+	for tempFirstPos*(tempFirstPos+1) < a {
+	  tempFirstPos++
+	  firstPos = tempFirstPos
 	}
 	
-	// finding n of nth term(Last valid term) in the range [A ... B] of type x*(x+1)
+	// finding n of nth term(Last valid term) of type  x*(x+1) in the range [A ... B]
 	tempLastPos := int(math.Floor(math.Sqrt(float64(b))))
 	lastPos := tempLastPos
-	if tempLastPos*(tempLastPos+1) > b {
-	  lastPos = tempLastPos-1
+	for tempLastPos*(tempLastPos+1) > b {
+	  tempLastPos--
+	  lastPos = tempLastPos
 	}
 	
 	
-	//fmt.Println("firstPos = ",firstPos, "firstElem = ", firstPos*(firstPos+1), "lastPos = ",lastPos, "lastElem = ", lastPos*(lastPos+1))
-	
-  //no of terms of type x*(x-1) becomes a simple substraction. 
-  return (lastPos - (firstPos - 1))
+  	//no of terms of type x*(x-1) becomes a simple substraction . 
+  	return (lastPos - (firstPos - 1))
 }
 
 func main() {
 	var a, b int = 0, 0
 	
-	// please enter 2 space, tab or new line seperated valid input integer values in STDIN
-	
-	fmt.Println("Enter Range Start value A = ")
+	// please enter 2 space or new line seperated values in STDIN
 	fmt.Scanf("%d", &a)
-	fmt.Println("Enter Range End value B = ")
 	fmt.Scanf("%d", &b)
 
-	
 	fmt.Println("Total terms = ", termCount(a,b))
 }
-
